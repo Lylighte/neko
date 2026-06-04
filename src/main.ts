403a943bdf1e8058c1be568c08bfdc15b64be90a
Lylighte <lylighte@ustc.edu>
2026-06-04 17:15:08 +0800

    refactor: remove toast and md-editor from app entry

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import './font/DefineFont.css'
 import App from './App.vue'
-import 'md-editor-v3/lib/style.css'
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
 import { router } from './router'
-
-import './theme-override/md-preview.css'
-import './theme-override/toast.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(Toast, {
-  transition: 'Vue-Toastification__bounce',
-  maxToasts: 20,
-  newestOnTop: true,
-  timeout: 3000,
-})
 
 app.mount('#app')
